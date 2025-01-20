61cdfb3aef247391eb3621bef29025c6e18bb035
Ashish Brahma <ashish.brahma@outlook.com>
2025-01-20 17:25:56 +0530

    Improve footer responsiveness and modify its content.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-	// TODO: Fetch using App Store Connect API.
-	var count = 60;
-	const stats = document.getElementById("stats");
-	if (count > 50) {
-		stats.querySelector('h2').innerHTML = count;
-	} else {
-		stats.style.display = "none";
+document.addEventListener("DOMContentLoaded", 
+	function getStats(event) {
+		// TODO: Fetch using App Store Connect API.
+		var count = 60;
+		const stats = document.getElementById("stats");
+		if (count > 50) {
+			stats.querySelector('h2').innerHTML = count;
+		} else {
+			stats.style.display = "none";
+		}
 	}
-});
+);
+
+document.addEventListener("DOMContentLoaded", 
+	function insertYear(event) {
+		const date = new Date();
+		const copyrights = document
+								.getElementById("copyrights")
+								.querySelector("small")
+								.querySelector("span");
+		copyrights.innerHTML = "&copy; " + date.getFullYear() + " KartStopper";
+	}
+);
